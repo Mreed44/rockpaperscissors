2adc023d9826fe2e9e8e508b5d4f83fb484954ab
Meg Reed <meg@scripted.org>
2018-10-26 18:33:41 +0000

    completed minimum required javascript for RPS

--- js/script.js
+++ js/script.js
@@ -3,9 +3,58 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
-
+/*global $*/
 
 
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice = ""
+var computerChoice = ""
+var winner = ""
+var randomNumber = 0;
 
+$("#shoot").click(function(){
+    userChoice = $("#input").val();
+    $("#userChoice").text(userChoice);
+    randomNumber = Math.random();
+    
+    if(randomNumber> .66) {
+        computerChoice = "ROCK";
+        $("#computerChoice").html("ROCK"); 
+    }
+    else if (randomNumber <.66 && randomNumber >.33){
+        computerChoice = "PAPER";
+        $("#computerChoice").html("PAPER"); 
+    }
+    else if (randomNumber <.33){
+        computerChoice = "SCISSORS";
+        $("#computerChoice").html("SCISSORS"); 
+    }
+    if(computerChoice == "ROCK" && userChoice == "ROCK"){
+        $("#result").html ("TIE!");
+    }
+    else if(computerChoice == "PAPER" && userChoice == "PAPER"){
+        $("#result").html ("TIE!");
+    }
+    else if(computerChoice == "SCISSORS" && userChoice == "SCISSORS"){
+        $("#result").html ("TIE!");
+    }
+    else if(computerChoice == "PAPER" && userChoice == "ROCK"){
+        $("#result").html ("COMPUTER WINS.");
+    }
+    else if(computerChoice == "SCISSORS" && userChoice == "PAPER"){
+        $("#result").html ("COMPUTER WINS.");
+    }
+    else if(computerChoice == "ROCK" && userChoice == "SCISSORS"){
+        $("#result").html ("COMPUTER WINS.");
+    }
+    else if(computerChoice == "SCISSORS" && userChoice == "ROCK"){
+        $("#result").html ("YOU WIN!");
+    }
+    else if(computerChoice == "ROCK" && userChoice == "PAPER"){
+        $("#result").html ("YOU WIN!");
+    }
+    else if(computerChoice == "PAPER" && userChoice == "SCISSORS"){
+        $("#result").html ("YOU WIN!");
+    }
+});
